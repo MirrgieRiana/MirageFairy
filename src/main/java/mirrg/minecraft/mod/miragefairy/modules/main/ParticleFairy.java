601efc3a8a39c0bb58729954e11d139f57106246
Mirrgie Riana <tacticsrealize2@gmail.com>
2017-04-04 03:37:00 +0900

    format

--- src/main/java/mirrg/minecraft/mod/miragefairy/modules/main/ParticleFairy.java
+++ src/main/java/mirrg/minecraft/mod/miragefairy/modules/main/ParticleFairy.java
@@ -6,18 +6,19 @@ import net.minecraft.world.World;
 public class ParticleFairy extends Particle
 {
 
+	{
+		particleScale /= 2;
+		particleMaxAge *= 4;
+	}
+
 	public ParticleFairy(World worldIn, double posXIn, double posYIn, double posZIn)
 	{
 		super(worldIn, posXIn, posYIn, posZIn);
-		particleScale /= 2;
-		particleMaxAge *= 4;
 	}
 
 	public ParticleFairy(World worldIn, double xCoordIn, double yCoordIn, double zCoordIn, double xSpeedIn, double ySpeedIn, double zSpeedIn)
 	{
 		super(worldIn, xCoordIn, yCoordIn, zCoordIn, xSpeedIn, ySpeedIn, zSpeedIn);
-		particleScale /= 2;
-		particleMaxAge *= 4;
 	}
 
 	@Override
